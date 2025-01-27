8a495de6dd1ca9f8c34648001ea164d7bb86478c
Jean-Michel Fayard <jmfayard@gmail.com>
2025-01-27 14:51:04 +0100

    Convert RentalsFormatter to Kotlin

--- src/main/java/movierental/RentalsFormatter.kt
+++ src/main/java/movierental/RentalsFormatter.kt
@@ -1,63 +1,53 @@
-package movierental;
+package movierental
 
-import org.jetbrains.annotations.NotNull;
+import java.util.stream.Collectors
 
-import java.util.stream.Collectors;
+class RentalsFormatter(val customer: Customer) {
 
-public class RentalsFormatter {
-    private final Customer customer;
-
-    public RentalsFormatter(Customer customer) {
-        this.customer = customer;
-    }
-
-    public String statement() {
+    fun statement(): String {
         // add bonus for a two day new release rental
-        int frequentRenterPoints = getFrequentRenterPoints();
-        double totalAmount = getTotalAmount();
-        String titles = rentalsTitles();
+        val frequentRenterPoints = getFrequentRenterPoints()
+        val totalAmount = getTotalAmount()
+        val titles = rentalsTitles()
 
-        return "Rental Record for " + customer.name + "\n" + titles + ("Amount owed is " + totalAmount + "\n") + ("You earned " + frequentRenterPoints + " frequent renter points\n");
+        return "Rental Record for " + customer.name + "\n" + titles + ("Amount owed is " + totalAmount + "\n") + ("You earned " + frequentRenterPoints + " frequent renter points\n")
     }
 
-    @NotNull
-    private String rentalsTitles() {
+    private fun rentalsTitles(): String {
         return customer.rentals.stream()
-                .map(each -> addTitleForLine(each, determineAmountsForLine(each)))
-                .collect(Collectors.joining());
+            .map<String> { each: Rental ->
+                addTitleForLine(
+                    each,
+                    determineAmountsForLine(each)
+                )
+            }
+            .collect(Collectors.joining())
     }
 
-    double getTotalAmount() {
+    fun getTotalAmount(): Double {
         return customer.rentals.stream()
-                .map(RentalsFormatter::determineAmountsForLine)
-                .reduce(0.0, Double::sum);
+            .map<Double> { each: Rental -> determineAmountsForLine(each) }
+            .reduce(0.0) { a, b -> a+b }
     }
 
-    int getFrequentRenterPoints() {
+    fun getFrequentRenterPoints(): Int {
         return customer.rentals.stream()
-                .map(Rental::rentalBonus)
-                .reduce(0, Integer::sum);
+            .map<Int> { obj: Rental -> obj.rentalBonus() }
+            .reduce(0) { a: Int, b: Int -> Integer.sum(a, b) }
     }
 
-    static String addTitleForLine(Rental each, double thisAmount) {
-        Movie movie = each.movie();
-        return String.format("\t%s\t%s\n", movie.title(), thisAmount);
-    }
+    companion object {
+        fun addTitleForLine(each: Rental, thisAmount: kotlin.Double): String {
+            val movie = each.movie
+            return String.format("\t%s\t%s\n", movie.title, thisAmount)
+        }
 
-    static double determineAmountsForLine(Rental each) {
-        return switch (each.movie().priceCode()) {
-            case PriceCode.REGULAR:
-                if (each.daysRented() > 2)
-                    yield 1.5 * each.daysRented() - 1;
-                else
-                    yield 2;
-            case PriceCode.NEW_RELEASE:
-                yield each.daysRented() * 3;
-            case PriceCode.CHILDRENS:
-                if (each.daysRented() > 3)
-                    yield 1.5 * each.daysRented() - 3;
-                else
-                    yield 1.5;
-        };
+        fun determineAmountsForLine(each: Rental): Double {
+            return when (each.movie.priceCode) {
+                PriceCode.REGULAR -> if (each.daysRented > 2) 1.5 * each.daysRented - 1.0 else 2.0
+                PriceCode.NEW_RELEASE -> each.daysRented * 3.0
+                PriceCode.CHILDRENS -> if (each.daysRented > 3) 1.5 * each.daysRented - 3.0 else 1.5
+            }
+        }
     }
 }
